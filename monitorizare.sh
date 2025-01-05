1927450b899f9c748b734d41666af7e11f2056e1
CristianC43 <craciunhoratiucristian@gmail.com>
2025-01-05 23:27:38 +0200

    Update monitorizare.sh

--- monitorizare.sh
+++ monitorizare.sh
@@ -1,10 +1,22 @@
-# Functie de monitorizare, primeste ca parametru un path
-
-
-# Luni fiecare saptamana ora 20:00
+# Functie de monitorizare
+# 2 parametri
+# $1 - path ul
+# $2 - tipul de comanda
 monitorizare(){
-	echo "0 20 * * 1 find $1 -mtime +60 -type f -exec rm {} \;" | crontab - 
+	case $2 in
+		1)
+			"echo 0 20 * * 1 find $1 -mtime +60 -type f -exec rm {} \;" | crontab - 
+			;;
+		2)
+			echo "0 20 * * 1 find $1 -mtime +1 -type f ! -name "*.old" -exec mv {} {}.old \;" | crontab  
+			;;
+		3)
+			echo "0 20 * * 1 find $1 -mtime +1 -type f -exec sed -i '1s/^/#### DEPERCATED ####\n/' {} \;" | crontab 
+			;;
+		*)
+			;;
+	esac
 }
 
-monitorizare ./lab3
 
+monitorizare ./tema1 3
