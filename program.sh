7a9b15a0b18ccab10780fd0399bf8883cd04e558
CristianC43 <craciunhoratiucristian@gmail.com>
2025-01-06 00:44:09 +0200

    Update program.sh

--- program.sh
+++ program.sh
@@ -48,10 +48,10 @@ monitorizare(){
 			"echo 0 20 * * 1 find $1 -mtime +60 -type f -exec rm {} \;" | crontab 
 			;;
 		2)
-			echo "0 20 * * 1 find $1 -mtime +1 -type f ! -name "*.old" -exec mv {} {}.old \;" | crontab  
+			echo "0 20 * * 1 find $1 -mtime +60 -type f ! -name "*.old" -exec mv {} {}.old \;" | crontab  
 			;;
 		3)
-			echo "0 20 * * 1 find $1 -mtime +1 -type f -exec sed -i '1s/^/#### DEPERCATED ####\n/' {} \;" | crontab 
+			echo "0 20 * * 1 find $1 -mtime +60 -type f -exec sed -i '1s/^/#### DEPERCATED ####\n/' {} \;" | crontab 
 			;;
 		*)
 			;;
