2be51822bfcd7b593d91eb68bde57a6d2ddbe029
CristianC43 <craciunhoratiucristian@gmail.com>
2025-01-07 00:12:30 +0200

    Update monitorizare.sh
    Problema la 3) inserare

--- monitorizare.sh
+++ monitorizare.sh
@@ -1,22 +1,19 @@
 # Functie de monitorizare
-# 2 parametri
+# Parametri:
 # $1 - path ul
 # $2 - tipul de comanda
 monitorizare(){
 	case $2 in
 		1)
-			"echo 0 20 * * 1 find $1 -mtime +60 -type f -exec rm {} \;" | crontab - 
+			echo "0 20 * * 1 find $1 -mtime +60 -type f -exec rm {} \;" | crontab 
 			;;
 		2)
-			echo "0 20 * * 1 find $1 -mtime +1 -type f ! -name "*.old" -exec mv {} {}.old \;" | crontab  
+			echo "0 20 * * 1 find $1 -mtime +60 -type f ! -name "*.old" -exec mv {} {}.old \;" | crontab  
 			;;
 		3)
-			echo "0 20 * * 1 find $1 -mtime +1 -type f -exec sed -i '1s/^/#### DEPERCATED ####\n/' {} \;" | crontab 
+			echo "0 20 * * 1 find $1 -mtime +60 -type f -exec sed -i '1s/^/#### DEPERCATED ####\n/' {} \;" | crontab 
 			;;
 		*)
 			;;
 	esac
 }
-
-
-monitorizare ./tema1 3
