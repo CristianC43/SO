1527d05487aaf065a8e99357c1c01b9d155d79ed
CristianC43 <craciunhoratiucristian@gmail.com>
2025-01-07 01:15:35 +0200

    Update program.sh
    update config+monitorizare

--- program.sh
+++ program.sh
@@ -48,10 +48,17 @@ monitorizare(){
 		3)
 			echo "0 20 * * 1 find $1 -mtime +60 -type f -exec sed -i '1s/^/#### DEPERCATED ####\n/' {} \;" | crontab 
 			;;
+		4)
+			echo "9 1 * * 2 find $1 -mtime +1 -type f -exec chmod u-x,g-x,o-x {} \;" | crontab
+			;;
+		5)
+			test -f archive.tar || touch archive.tar # Se va creea o arhiva daca nu exista deja
+			echo "0 20 * * 1 find $1 -mtime +60 -type f -exec tar -rvf archive.tar {} \;" | crontab
+			;;			
 		*)
 			;;
 	esac
-}
+
 
 
 # Functie meniu de configurare
@@ -62,9 +69,11 @@ config(){
 	    opt1="Stergere fisiere"
 	    opt2="Redenumire fisiere extensia .old"
 	    opt3="Adaugare linie de ### DEPERCATED ### pe prima linie a fisierelor"
-	    opt4="Inapoi"
+		opt4="Luare permisiune executare"
+		opt5="Arhivare fisiere"
+	    optEXIT="Inapoi"
 
-	    select optiune in "$opt1" "$opt2" "$opt3" "$opt4"; do
+	    select optiune in "$opt1" "$opt2" "$opt3" "$opt4" "$opt5" "$optEXIT"; do
 	    	case $REPLY in
 	        	1)	# Stergere fisiere
 					monitorizare $1 1 
@@ -81,10 +90,21 @@ config(){
 					echo "Monitorizarea directorului $1 a inceput"
 	                break
 	                ;;
-	            4)	# Inapoi
+	            4)
+	            	monitorizare $1 4
+	            	echo "Monitorizarea directorului $1 a inceput"
+	                break
+	                ;;
+	            5)
+	            	monitorizare $1 5
+	           	 	echo "Monitorizarea directorului $1 a inceput"
+	                break
+	                ;;
+	            6)	# Inapoi
 	            	echo "Monitorizare anulata"
 					break
 	                ;;
+
 	            *)
 	                echo "Cod de configurare invalid" >&2
 	                ;;
